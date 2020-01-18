c32e54e985d9f8be941aed72da96daa9b4c74e66
Pawel <ssopel10@gmail.com>
2020-01-18 17:14:38 +0100

    add mobile view

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const header = document.querySelector('.header');
 const orderBtn = document.querySelector('.catering__order-button');
 const closeBtn = document.querySelector('.form__close-button');
 const bookForm = document.querySelector('.book-order');
+const menuBtn = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
 
 const scrollToSection = () => {
   window.scrollTo({
@@ -25,7 +27,14 @@ const showBookForm = () => {
   document.body.classList.toggle('lockScroll');
 };
 
+const showNav = () => {
+  menuBtn.classList.toggle('is-active');
+  nav.classList.toggle('visible');
+  document.body.classList.toggle('lockScroll');
+};
+
 window.addEventListener('scroll', lockHeader);
 mouse.addEventListener('click', scrollToSection);
 orderBtn.addEventListener('click', showBookForm);
 closeBtn.addEventListener('click', showBookForm);
+menuBtn.addEventListener('click', showNav);
