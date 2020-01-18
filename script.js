b64befe8bbd60c602b5604d73866daacbe0dd6fc
Pawel <ssopel10@gmail.com>
2020-01-18 14:12:00 +0100

    install eslint

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-const mouse = document.querySelector(".home__mouse-icon");
-const firstSection = document.querySelector(".about");
-const header = document.querySelector(".header");
-const orderBtn = document.querySelector(".catering__order-button");
-const closeBtn = document.querySelector(".form__close-button");
-const bookForm = document.querySelector(".book-order");
+const mouse = document.querySelector('.home__mouse-icon');
+const firstSection = document.querySelector('.about');
+const header = document.querySelector('.header');
+const orderBtn = document.querySelector('.catering__order-button');
+const closeBtn = document.querySelector('.form__close-button');
+const bookForm = document.querySelector('.book-order');
 
 const scrollToSection = () => {
-    window.scrollTo({
-        top: firstSection.offsetTop,
-        left: 0,
-        behavior: 'smooth'
-      });
-}
+  window.scrollTo({
+    top: firstSection.offsetTop,
+    left: 0,
+    behavior: 'smooth',
+  });
+};
 
 const lockHeader = () => {
-    if(window.scrollY>header.offsetHeight){
-        header.classList.add('fixed')
-    }
-    else header.classList.remove('fixed')
-}
+  if (window.scrollY > header.offsetHeight) {
+    header.classList.add('fixed');
+  } else header.classList.remove('fixed');
+};
 
 const showBookForm = () => {
-    bookForm.classList.toggle('visible');
-    bookForm.style.top=`${window.scrollY}px`;
-    document.body.classList.toggle('lockScroll');
-}
+  bookForm.classList.toggle('visible');
+  bookForm.style.top = `${window.scrollY}px`;
+  document.body.classList.toggle('lockScroll');
+};
 
 window.addEventListener('scroll', lockHeader);
 mouse.addEventListener('click', scrollToSection);
